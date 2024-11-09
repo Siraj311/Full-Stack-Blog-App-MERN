990affba5b49b6c0a68b716e05d218dd3ccfa331
Siraj Preena <sirajpreena311@gmail.com>
2024-11-09 12:37:15 +0530

    Adding Edit Post Function

--- api/index.js
+++ api/index.js
@@ -108,4 +108,35 @@ app.get('/post/:id', async (req, res) => {
   res.json(postDoc);
 })
 
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+  let newPath = null;
+  if(req.file) {
+    const {originalname, path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length -1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if(err) throw err;
+    const {id, title, summary, content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = postDoc._id.equals(id);
+    if(!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+
+    await postDoc.update({
+      title,
+      summary, 
+      content,
+      cover: newPath ? newPath : postDoc.cover
+    })
+
+    res.json(postDoc);
+  })
+})
+
 app.listen(4000);
